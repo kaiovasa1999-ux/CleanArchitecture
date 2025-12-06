c2b137e8de06a29bb670ca07adcaca8c94881c19
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2025-12-06 17:48:00 +0200

    test

--- Gatherly/src/Gatherly.Application/Abstractions/IEmailService.cs
+++ Gatherly/src/Gatherly.Application/Abstractions/IEmailService.cs
@@ -6,5 +6,5 @@ public interface IEmailService
 {
     Task SendInvitationSentEmailAsync(Member member, Gathering gathering, CancellationToken cancellationToken = default);
 
-    Task SendInvitationAcceptedEmailAsync(Gathering gathering, CancellationToken cancellationToken = default);
+    Task SendInvitationAcceptedEmailAsync(Gathering  gathering, CancellationToken cancellationToken = default);
 }
